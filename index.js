93a44544aa8b5da3aada28fc1e804187c9f4343f
Denis Cheruiyot <Cheruiyotkp974@gmail.com>
2025-03-04 17:20:48 +0300

    modified text

--- index.js
+++ index.js
@@ -1,5 +1,24 @@
-
-
 console.log(2 + 2);
-console.log(2 + (3 - 5))
-console.log("Hello World!");
+console.log(5 + (14 - 39));
+console.log(2 + (3 - 5));
+console.log("Hello World");
+
+let goat = "Lionel";
+console.log(goat);
+
+let years = 25;
+console.log(years);
+console.log(typeof years);
+
+let game = true;
+console.log(game);
+
+const person = { firstName: "Denis", lastName: "Cheruiyot" };
+console.log(person);
+
+const car = { Head: "Mercedes", Tail: "Benz" };
+console.log(car);
+
+const playeryears = { player: "true", years: "27" };
+console.log(playeryears);
+
